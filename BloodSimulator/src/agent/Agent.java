72618c10c555b4597fe6554d5ecbf7e11552c9e8
Julien-laville <alternative451@gmail.com>
2013-10-29 23:32:57 +0100

    init

---
+++ BloodSimulator/src/agent/Agent.java
@@ -0,0 +1,15 @@
+package agent;
+
+import java.awt.Point;
+import java.util.List;
+
+
+public class Agent {
+	private static enum TEAM_COLOR{RED, BLUE};
+	private Point position;
+	private int hp;
+	private int mana;
+	private List<Modifieur> modifieurs;
+	
+
+}
---
+++ BloodSimulator/src/agent/Modifieur.java
@@ -0,0 +1,5 @@
+package agent;
+
+public interface Modifieur {
+	public void apply(Agent agent);
+}
